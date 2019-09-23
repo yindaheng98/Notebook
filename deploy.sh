e83a8c61a148f003b10b5dd975796c8afa1cf543
yindaheng98 <yindaheng98@163.com>
2019-09-23 17:21:35 +0800

    fix 'nothing to commit' in CI

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,7 @@ rm -rf yindaheng98.github.io/source/_posts
 cp -r 学习笔记 yindaheng98.github.io/source/_posts
 cd yindaheng98.github.io
 git add -A
+set +e
 git commit -m "TravisCI push"
+set -e
 git push -u https://$1@github.com/yindaheng98/yindaheng98.github.io.git HEAD:source --force
