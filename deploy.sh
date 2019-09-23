d76f66809e6c5f1e9240289e91c751fe0ed00a1e
yindaheng98 <yindaheng98@163.com>
2019-09-23 16:04:05 +0800

    CI bug fixed

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ git config user.name "TravisCI"
 git config user.email "yindaheng98@163.com"
 set -e
 git clone https://github.com/yindaheng98/yindaheng98.github.io.git
--rm -rf yindaheng98.github.io/source/_posts
+rm -rf yindaheng98.github.io/source/_posts
 cp -r 学习笔记 yindaheng98.github.io/source/_posts
 cd yindaheng98.github.io
 git add -A
