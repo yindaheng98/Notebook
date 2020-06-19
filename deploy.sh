e2fa960a9f89f613a04874fc9008ee09a8c57a6f
yindaheng98 <yindaheng98@163.com>
2020-06-19 23:50:22 +0800

    部署方式更新

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git config user.name "TravisCI"
 git config user.email "yindaheng98@163.com"
 set -e
-git clone https://github.com/yindaheng98/yindaheng98.github.io.git
+git clone -b dev https://github.com/yindaheng98/yindaheng98.github.io.git
 rm -rf yindaheng98.github.io/source/_posts
 cp -r 学习笔记 yindaheng98.github.io/source/_posts
 cd yindaheng98.github.io
