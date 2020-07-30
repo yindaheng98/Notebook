3976fcf3ac9e8bc4940b0db6c6dbd90115e3fae2
yindaheng98 <yindaheng98@163.com>
2020-07-30 15:13:57 +0800

    bug fixed

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone -b dev https://github.com/yindaheng98/yindaheng98.github.io.git
-cp -r 学习笔记 yindaheng98.github.io/blogs
-cp -r 学习笔记 yindaheng98.github.io/blogs/.vuepress/public
+cp -r 学习笔记/. yindaheng98.github.io/blogs
+cp -r 学习笔记/. yindaheng98.github.io/blogs/.vuepress/public
 cd yindaheng98.github.io
 echo "开始编译"
 tree
