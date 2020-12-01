750cd8c4c73550aaaf28a85042fa81d0e72577e1
yindaheng98 <yindaheng98@163.com>
2020-12-01 22:00:07 +0800

    bug fix

--- build-simple-blog.sh
+++ build-simple-blog.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-echo "$1" | docker login -u "yindaheng98" --password-stdin
 git clone -b master https://github.com/yindaheng98/simple-blog.git
 cp -r 学习笔记/. simple-blog/md
 cd simple-blog
 echo "开始编译"
 tree
+echo "$1" | docker login -u "yindaheng98" --password-stdin
 docker build -t yindaheng98/simple-blog:latest .
 docker push yindaheng98/simple-blog:latest
-docker login --username yindaheng98
 cd ..
 rm -rf simple-blog
