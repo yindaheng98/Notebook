4e5282fb85069a115786919b17a38d01e65c2221
yindaheng98 <yindaheng98@163.com>
2022-07-12 20:18:26 +0800

    new feature

--- 知乎转Markdown.py
+++ 知乎转Markdown.py
@@ -3,7 +3,7 @@ import brotli
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import unquote
-link = "https://zhuanlan.zhihu.com/p/348498294"
+link = "https://zhuanlan.zhihu.com/p/359366717"
 response = requests.get(link, headers={
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "accept-encoding": "gzip, deflate, br",
@@ -25,21 +25,30 @@ content = str(soup.body.div.div.article.find_all("div", class_="Post-RichTextCon
 content = re.sub(r'<br/>', "\n", content)
 content = re.sub(r'\n', "", content)
 
+content = re.sub(r'<code>(.*?)</code>', lambda m:" `%s` " % m.group(1), content)
 content = re.sub(r'<b>(.*?)</b>', lambda m:" **%s** " % m.group(1), content)
 content = re.sub(r'<a[^>]*href="https://link.zhihu.com/\?target=([^">]+?)"[^>]*>(.*?)</a>', lambda m:"[%s](%s)" % (m.group(2), unquote(m.group(1))), content)
 content = re.sub(r'<a[^>]*href="([^">]+?)"[^>]*>(.*?)</a>', lambda m:"[%s](%s)" % (m.group(2), m.group(1)), content)
 
 content = re.sub(r'<source[^>]*/>', "", content)
-content = re.sub(r'<picture><img[^>]*alt="([^"]+?)"[^>]*/></picture>', lambda m:"$$%s$$" % m.group(1), content)
+content = re.sub(r'<picture><img[^>]*alt="([^"]+?)"[^>]*/></picture>', lambda m:"\n$$%s$$\n" % m.group(1), content)
 content = re.sub(r'<img[^>]*alt="([^"]+?)"[^>]*/>', lambda m:"$%s$" % m.group(1), content)
 
 def fig(m):
-    return re.sub(r'<noscript><img[^>]*data-original="([^"]+?)"[^>]*/></noscript><img[^>]*>', lambda mm:"\n![](%s)\n" % mm.group(1), m.group(1))
+    captions = re.findall(r'<figcaption>([^<]+?)</figcaption>', m.group(1))
+    caption = captions[0] if len(captions) > 0 else ''
+    c = re.sub(r'<noscript><img[^>]*data-original="([^"]+?)"[^>]*/></noscript><img[^>]*>', lambda mm:"\n![%s](%s)\n" % (caption, mm.group(1)), m.group(1))
+    c = re.sub(r'<figcaption>([^<]+?)</figcaption>', '', c)
+    return c
 content = re.sub(r'<figure[^>]*>(.*?)</figure>', fig, content)
 
 def ol(m):
     return re.sub(r'<li[^>]*>(.*?)</li>', lambda mm:"1. %s\n" % mm.group(1), m.group(1))
 content = re.sub(r'<ol>(.*?)</ol>', ol, content)
+
+def ul(m):
+    return re.sub(r'<li[^>]*>(.*?)</li>', lambda mm:"* %s\n" % mm.group(1), m.group(1))
+content = re.sub(r'<ul>(.*?)</ul>', ol, content)
 
 content = re.sub(r'<blockquote[^>]*>([^<]*?)</blockquote>', lambda m:"\n>%s\n" % m.group(1), content)
 content = re.sub(r'<p[^>]*>([^<]*?)</p>', lambda m:"\n%s\n" % m.group(1), content)
